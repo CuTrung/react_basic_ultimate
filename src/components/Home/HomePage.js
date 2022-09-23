3a07fc9c99c9e1db84f7ae8f414c37c4dab75bba
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-09-23 22:11:58 +0700

    #52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,6 +1,16 @@
+import videoHomepage from '../../assets/video-homepage.mp4';
+
+
 const HomePage = () => {
     return (
-        <div className="homepage-container">home page component</div>
+        <div className="homepage-container">
+            <video autoPlay muted loop>
+                <source
+                    src={videoHomepage}
+                    type="video/mp4" />
+
+            </video>
+        </div>
     )
 }
 
