4b484bc3e92ca527723219c962affa139891befb
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-09-26 20:18:00 +0700

    #70

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,18 +1,36 @@
 import { useState } from 'react';
 import './Login.scss';
+import { useNavigate } from 'react-router-dom';
+import { postLogin } from '../../services/apiServices';
+import { toast } from 'react-toastify';
+
+
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassWord] = useState('');
 
-    const handleLogin = () => {
+    const navigate = useNavigate();
 
+    const handleLogin = async () => {
+        //validate
+
+        //submit apis
+        let data = await postLogin(email, password);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
+            navigate('/');
+        }
+
+        if (data && +data.EC !== 0) {
+            toast.error(data.EM);
+        }
     }
 
     return (
         <div className="login-container">
             <div className='header'>
-                Don't have an account yet?
-
+                <span> Don't have an account yet?</span>
+                <button>Sign up</button>
             </div>
             <div className='title col-4 mx-auto'>
                 HoiDanIT
@@ -41,8 +59,15 @@ const Login = (props) => {
                 </div>
                 <span className='forgot-password'>Forgot password ?</span>
                 <div>
-                    <button onClick={() => handleLogin()} className='btn-submit'>Login to HoiDanIT</button>
+                    <button
+                        className='btn-submit'
+                        onClick={() => handleLogin()}
+                    >Login to HoiDanIT</button>
                 </div>
+                <div className='text-center'>
+                    <span className='back' onClick={() => navigate('/')}> &#60; &#60; Go to Homepage</span>
+                </div>
+
             </div>
 
         </div>
