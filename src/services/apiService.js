e12899daebb193aefe9136f6f089d08d327c46b8
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-09-28 21:02:49 +0700

    #82

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -53,11 +53,15 @@ const getQuizByUser = () => {
     return axios.get('api/v1/quiz-by-participant');
 }
 
+const getDataQuiz = (id) => {
+    return axios.get(`api/v1/questions-by-quiz?quizId=${id}`);
+}
+
 
 
 
 export {
     postCreateNewUser, getAllUsers,
     putUpdateUser, deleteUser, getUserWithPaginate,
-    postLogin, postRegister, getQuizByUser
+    postLogin, postRegister, getQuizByUser, getDataQuiz
 };
