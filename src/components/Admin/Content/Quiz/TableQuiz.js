f953fa40b7bd81d1d30e846fac2cf09b91fc1e00
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-09-30 21:15:13 +0700

    #93

--- src/components/Admin/Content/Quiz/TableQuiz.js
+++ src/components/Admin/Content/Quiz/TableQuiz.js
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import { getAllQuizForAdmin } from "../../../../services/apiService";
+import ModalDeleteQuiz from "./ModalDeleteQuiz";
+import ModalUpdateQuiz from "./ModalUpdateQuiz";
 
 const TableQuiz = (props) => {
     const [listQuiz, setListQuiz] = useState([]);
+    const [showModalDeleteUser, setShowModalDeleteUser] = useState(false);
+    const [dataDelete, setDataDelete] = useState({});
+
+    const [showModalUpdateUser, setShowModalUpdateUser] = useState(false);
+    const [dataUpdate, setDataUpdate] = useState({});
 
     useEffect(() => {
         fetchQuiz();
@@ -14,6 +21,22 @@ const TableQuiz = (props) => {
             setListQuiz(res.DT);
         }
     }
+
+    const handleClickBtnDelete = (quiz) => {
+        setShowModalDeleteUser(true);
+        setDataDelete(quiz);
+    }
+
+    const handleClickBtnUpdate = (quiz) => {
+        setShowModalUpdateUser(true);
+        setDataUpdate(quiz);
+    }
+
+    const resetUpdateData = () => {
+        setDataUpdate({})
+    }
+
+
 
     return (
         <>
@@ -31,20 +54,45 @@ const TableQuiz = (props) => {
                 <tbody>
                     {listQuiz && listQuiz.map((item, index) => {
                         return (
-                            <tr key={`table-quiz-${index}`}>
-                                <th>{item.id}</th>
-                                <td>{item.name}</td>
-                                <td>{item.description}</td>
-                                <td>{item.difficulty}</td>
-                                <td style={{ display: "flex", gap: "15px" }}>
-                                    <button className="btn btn-warning">Edit</button>
-                                    <button className="btn btn-danger">Delete</button>
-                                </td>
-                            </tr>
+                            <>
+                                <tr key={`table-quiz-${index}`}>
+                                    <th>{item.id}</th>
+                                    <td>{item.name}</td>
+                                    <td>{item.description}</td>
+                                    <td>{item.difficulty}</td>
+                                    <td style={{ display: "flex", gap: "15px" }}>
+                                        <button
+                                            className="btn btn-warning"
+                                            onClick={() => handleClickBtnUpdate(item)}
+                                        >Edit</button>
+                                        <button
+                                            className="btn btn-danger"
+                                            onClick={() => handleClickBtnDelete(item)}
+                                        >Delete</button>
+                                    </td>
+                                </tr>
+
+                                <ModalDeleteQuiz
+                                    show={showModalDeleteUser}
+                                    setShow={setShowModalDeleteUser}
+                                    dataDelete={dataDelete}
+                                    fetchQuiz={fetchQuiz}
+                                />
+
+                                <ModalUpdateQuiz
+                                    show={showModalUpdateUser}
+                                    setShow={setShowModalUpdateUser}
+                                    dataUpdate={dataUpdate}
+                                    fetchQuiz={fetchQuiz}
+                                    resetUpdateData={resetUpdateData}
+                                />
+                            </>
                         )
                     })}
                 </tbody>
             </table>
+
+
         </>
     )
 }
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -75,10 +75,27 @@ const getAllQuizForAdmin = () => {
     return axios.get(`api/v1/quiz/all`);
 }
 
+const deleteQuizForAdmin = (id) => {
+    return axios.delete(`api/v1/quiz/${id}`);
+}
+
+const putUpdateQuiz = (id, description, name, difficulty, quizImage) => {
+    // submit data
+    const data = new FormData();
+    data.append('id', id);
+    data.append('description', description);
+    data.append('name', name);
+    data.append('difficulty', difficulty);
+    data.append('quizImage', quizImage);
+
+    return axios.put('api/v1/quiz', data);
+}
+
 
 export {
     postCreateNewUser, getAllUsers,
     putUpdateUser, deleteUser, getUserWithPaginate,
     postLogin, postRegister, getQuizByUser, getDataQuiz,
-    postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin
+    postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin,
+    deleteQuizForAdmin, putUpdateQuiz
 };
