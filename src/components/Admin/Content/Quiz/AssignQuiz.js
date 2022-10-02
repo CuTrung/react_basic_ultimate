a5dc3302fa6eae1f5fd06e03aeea0efaa3683325
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-10-02 10:04:23 +0700

    #101

--- src/components/Admin/Content/Quiz/AssignQuiz.js
+++ src/components/Admin/Content/Quiz/AssignQuiz.js
@@ -1,6 +1,7 @@
 import Select from 'react-select';
 import { useState, useEffect } from 'react';
-import { getAllQuizForAdmin, getAllUsers } from '../../../../services/apiService';
+import { getAllQuizForAdmin, getAllUsers, postAssignQuiz } from '../../../../services/apiService';
+import { toast } from 'react-toastify';
 
 const AssignQuiz = (props) => {
 
@@ -21,7 +22,7 @@ const AssignQuiz = (props) => {
             let newQuiz = res.DT.map(item => {
                 return {
                     value: item.id,
-                    label: `${item.id} - ${item.description}`
+                    label: `${item.id} - ${item.name}`
                 }
             })
             setListQuiz(newQuiz);
@@ -38,6 +39,15 @@ const AssignQuiz = (props) => {
                 }
             })
             setListUser(users);
+        }
+    }
+
+    const handleAssign = async () => {
+        let rs = await postAssignQuiz(selectedQuiz.value, selectedUser.value);
+        if (rs && rs.EC === 0) {
+            toast.success(rs.EM);
+        } else {
+            toast.error(rs.EM);
         }
     }
 
@@ -61,7 +71,10 @@ const AssignQuiz = (props) => {
                 />
             </div>
             <div>
-                <button className='btn btn-warning mt-3'>Assign</button>
+                <button
+                    className='btn btn-warning mt-3'
+                    onClick={() => handleAssign()}
+                >Assign</button>
             </div>
         </div>
     )
--- src/components/Admin/Content/Quiz/QuizQA.js
+++ src/components/Admin/Content/Quiz/QuizQA.js
@@ -11,7 +11,7 @@ import _ from 'lodash';
 import Lightbox from "react-awesome-lightbox";
 import {
     getAllQuizForAdmin, postCreateNewQuestionForQuiz,
-    postCreateNewAnswerForQuiz
+    postCreateNewAnswerForQuiz, getQuizWithQA
 } from "../../../../services/apiService";
 import { toast } from 'react-toastify';
 
@@ -48,6 +48,39 @@ const QuizQA = (props) => {
     useEffect(() => {
         fetchQuiz();
     }, [])
+
+    useEffect(() => {
+        if (selectedQuiz && selectedQuiz.value) {
+            fetchQuizWithQA();
+        }
+    }, [selectedQuiz])
+
+    //return a promise that resolves with a File instance
+    function urltoFile(url, filename, mimeType) {
+        return (fetch(url)
+            .then(function (res) { return res.arrayBuffer(); })
+            .then(function (buf) { return new File([buf], filename, { type: mimeType }); })
+        );
+    }
+
+
+    const fetchQuizWithQA = async () => {
+        let rs = await getQuizWithQA(selectedQuiz.value);
+        if (rs && rs.EC === 0) {
+            // convert base64 to File object
+            let newQA = [];
+            for (let i = 0; i < rs.DT.qa.length; i++) {
+                let q = rs.DT.qa[i];
+                if (q.imageFile) {
+                    q.imageName = `Question-${q.id}.png`;
+                    q.imageFile =
+                        await urltoFile(`data:image/png;base64,${q.imageFile}`, `Question-${q.id}.png`, 'image/png');
+                }
+                newQA.push(q);
+            }
+            setQuestions(newQA);
+        }
+    }
 
     const fetchQuiz = async () => {
         let res = await getAllQuizForAdmin();
