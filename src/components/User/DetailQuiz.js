46ab246da1883a696536be9ff96aae90bdf1ad81
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-10-02 14:55:26 +0700

    #104

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -162,6 +162,7 @@ const DetailQuiz = (props) => {
             <div className='right-content'>
                 <RightContent
                     dataQuiz={dataQuiz}
+                    handleFinishQuiz={handleFinishQuiz}
                 />
             </div>
 
